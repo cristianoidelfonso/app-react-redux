c354ae54e841dcb014e1e59b30d8ad70b64382b0
Cristiano Idelfonso <cristiano_idelfonso@hotmail.com>
2022-03-18 19:17:28 -0300

    Salvando apenas o token no localstorage

--- src/services/auth.service.jsx
+++ src/services/auth.service.jsx
@@ -20,7 +20,7 @@ const login = (email, password) => {
     })
     .then((response) => {
       if (response.data.accessToken) {
-        localStorage.setItem("user", JSON.stringify(response.data));
+        localStorage.setItem("user", JSON.stringify(response.data.accessToken));
       }
       return response.data;
     });
